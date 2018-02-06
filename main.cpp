fce686928b64de7b6a776c58a02dd22cc0314390
shemuelong <31332054+shemuelong@users.noreply.github.com>
2018-02-05 16:37:17 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 #include <cmath>
+double function(double x);
+//This function implements the function in the picture (an arbitrary function)
+
+double bisection(double lower, double upper);
+//This recursive function "bisection" takes two values and finds a root (if it exists) between the two
+
+
+
+int main() {    //From the given picture, we can see that there are two roots.
+                //The Function bisection, which finds the roots must run once for each of the two roots
+                //The two ranges that will be used is 4 to 7 and 7 to 10.
+                //If we were to use invalid ranges, the error message would simply appear.
+    std::cout << "First root is: " << bisection(4,7) << std::endl;      //Function finds and outputs the first root between 4 and 7
+    std::cout << "Second root is: " << bisection(7,10) << std::endl;     //Function finds and outputs the second root between 7 and 10
+
+    return 0;
+}
 
 double function(double x){
     //This function implements the function in the picture (an arbitrary function)
-    //<cmath> is included because it is a sine function.
+    //<cmath> is included for the sine function.
     const double PI = 3.14159;
     return (sin(x + PI/4)); //return value simply follows the function given in the midterm question
 }
@@ -22,14 +39,4 @@ double bisection(double lower, double upper){ //This recursive function "bisecti
         return bisection(lower, middle);             //The recursive call narrows the range from (lower,upper) to (lower,middle)
     else if (function(middle) * function(lower) > 0) //If the root lies within middle and lower
         return bisection(middle, upper);             //The recursive call narrows the range to (middle, upper)
-}
-
-int main() {    //From the given picture, we can see that there are two roots.
-                //The Function bisection, which finds the roots must run once for each of the two roots
-                //The two ranges that will be used is 4 to 7 and 7 to 10.
-                //If we were to use invalid ranges, the error message would simply appear.
-    std::cout << "First root is: " << bisection(4,7) << std::endl;      //Function finds and outputs the first root between 4 and 7
-    std::cout << "Second root is: " << bisection(7,10) << std::endl;     //Function finds and outputs the second root between 7 and 10
-
-    return 0;
 }
